f231884ae00850e33498f0af9d9b3d70ce3c3c90
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-07-16 22:29:05 +1000

    Added triumph static library

---
+++ Triumph/Triumph/TRTriumph.m
@@ -0,0 +1,43 @@
+//
+//  Triumph.m
+//  Triumph
+//
+//  Created by Stewart Gleadow on 16/07/12.
+//  Copyright (c) 2012 Stewart Gleadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <dlfcn.h>
+
+@interface TRTriumph : NSObject
+@end
+
+@implementation TRTriumph
+
++ (void)load
+{
+    void *loadedTests = NULL;
+    loadedTests = dlopen("/Users/sgleadow/Dropbox/Documents/Tech/Testing/unit-test-runner/output/TriumphSampleTests.octest/TriumphSampleTests", RTLD_NOW);
+    assert(loadedTests != NULL);
+    
+    // Try and load one of the test classes
+    // TODO: this check needs to go in the final version
+    if (NSClassFromString(@"TriumphSampleTests"))
+    {
+        printf("Could find test\n");
+    }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:[self class] 
+                                             selector:@selector(applicationDidBecomeActive:) 
+                                                 name:@"UIApplicationDidBecomeActiveNotification" 
+                                               object:nil];
+}
+
++ (void)applicationDidBecomeActive:(NSNotification *)notification
+{
+    NSLog(@"\n\n\n==============\nLibrary loaded and automatically hooked into applicationDidBecomeActive\n==============\n\n\n");
+    
+    SenSelfTestMain();
+}
+
+@end
---
+++ Triumph/TriumphTests/TriumphTests.m
@@ -0,0 +1,32 @@
+//
+//  TriumphTests.m
+//  TriumphTests
+//
+//  Created by Stewart Gleadow on 16/07/12.
+//  Copyright (c) 2012 Stewart Gleadow. All rights reserved.
+//
+
+#import "TriumphTests.h"
+
+@implementation TriumphTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TriumphTests");
+}
+
+@end
