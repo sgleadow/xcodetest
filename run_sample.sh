75daee42585ab79bfd825dd274d00717c55c97e7
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-07-19 09:45:09 +1000

    Sample shell script to show how to use the build in script

---
+++ run_sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Sample script to use the test runner to use XcodeTest
+
+if [ ! -f libXcodeTest.a ]; then
+    echo "libXcodeTest.a does not exist in the current directory"
+    echo "use \"make xcodetest\" to build it"
+    exit 1
+fi
+
+# To use the script, feed in the scheme names (which are also assumed to match the build target names)
+./build_and_run_unit_tests.sh TriumphSample TriumphSampleTests
