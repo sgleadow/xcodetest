38bd5a50926d60bc47b0def7ed1798705da094c9
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-07-19 15:27:18 +1000

    Don't need a separate method for this anymore

--- XcodeTest/SGXcodeTest.m
+++ XcodeTest/SGXcodeTest.m
@@ -17,21 +17,17 @@
 
 + (void)load
 {
+    char *unitTestPath = getenv("XCODE_TEST_PATH");
+    assert(unitTestPath != NULL);
+    
     void *loadedTests = NULL;
-    loadedTests = dlopen([self unitTestObjectFilePath], RTLD_NOW);
+    loadedTests = dlopen(unitTestPath, RTLD_NOW);
     assert(loadedTests != NULL);
     
     [[NSNotificationCenter defaultCenter] addObserver:[self class] 
                                              selector:@selector(applicationDidBecomeActive:) 
                                                  name:@"UIApplicationDidBecomeActiveNotification" 
                                                object:nil];
-}
-
-+ (char *)unitTestObjectFilePath
-{
-    char *unitTestPath = getenv("XCODE_TEST_PATH");
-    assert(unitTestPath != NULL);
-    return unitTestPath;
 }
 
 + (void)applicationDidBecomeActive:(NSNotification *)notification
