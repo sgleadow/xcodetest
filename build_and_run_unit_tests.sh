ef0b9b5fa7545d5cf79fde7d99227a067a2d735c
Stewart Gleadow <sgleadow@thoughtworks.com>
2012-09-28 19:41:16 +1000

    Print command before running it

--- build_and_run_unit_tests.sh
+++ build_and_run_unit_tests.sh
@@ -36,6 +36,7 @@ if [[ $? != 0 ]]; then
 fi
 
 # Build the main app, with libXcodeTest.a linked in
+echo "xcodebuild -sdk iphonesimulator -scheme ${MAIN_APP_TARGET} build CONFIGURATION_BUILD_DIR=\"${OUTPUT_DIR}\" XCODE_TEST_LDFLAGS=\"${XCODE_TEST_LDFLAGS}\""
 xcodebuild -sdk iphonesimulator -scheme ${MAIN_APP_TARGET} build CONFIGURATION_BUILD_DIR="${OUTPUT_DIR}" XCODE_TEST_LDFLAGS="${XCODE_TEST_LDFLAGS}"
 if [[ $? != 0 ]]; then
   echo "Failed to build app!"
