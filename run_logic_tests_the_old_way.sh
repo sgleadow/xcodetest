56b53a64cd56d4e7947fed78fc47d93e3d6e11b3
Stewart Gleadow <sgleadow@gmail.com>
2012-11-27 17:49:38 +1100

    Getting logic tests running the old way

--- run_logic_tests_the_old_way.sh
+++ run_logic_tests_the_old_way.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This is how I used to run the unit tests. Worked pre-Xcode 4.5
 
-xcodebuild -sdk iphonesimulator -scheme XcodeTestSamplePassingTests build TEST_AFTER_BUILD=YES
+xcodebuild -sdk iphonesimulator -scheme XcodeTestSampleNonUITests build ONLY_ACTIVE_ARCH=NO TEST_AFTER_BUILD=YES
